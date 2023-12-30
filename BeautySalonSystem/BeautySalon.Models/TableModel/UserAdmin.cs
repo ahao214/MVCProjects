be385d823fa2d2ab6525c24918e26d0785a66272
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-30 21:27:12 +0800

    add model

---
+++ BeautySalonSystem/BeautySalon.Models/TableModel/UserAdmin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BeautySalon.Models.TableModel
+{
+    public class UserAdmin
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string RealName { get; set; }
+        public string Telphone { get; set; }
+        public string LoginIP { get; set; }
+        public DateTime LoginTime { get; set; }
+        public DateTime ModifyTime { get; set; }
+        public decimal Salary { get; set; }
+        public int UserStatus { get; set; }
+
+    }
+}
