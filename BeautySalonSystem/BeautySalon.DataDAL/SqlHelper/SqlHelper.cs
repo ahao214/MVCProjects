a3c65c1be0e3904ddc7a3985cecffbd09431a8d8
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-30 22:00:39 +0800

    数据类库

---
+++ BeautySalonSystem/BeautySalon.DataDAL/SqlHelper/SqlHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySalon.DataDAL.SqlHelper
+{
+    /// <summary>
+    /// 抽象数据库帮助类
+    /// </summary>
+    public abstract  class SqlHelper
+    {
+
+
+    }
+}
