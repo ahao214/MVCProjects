c5849b2cfa37602efac531e98cdcf5a6b6312030
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-30 22:12:37 +0800

    数据操作

--- BeautySalonSystem/BeautySalon.DataDAL/SqlHelper/SqlHelper.cs
+++ BeautySalonSystem/BeautySalon.DataDAL/SqlHelper/SqlHelper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +12,41 @@ namespace BeautySalon.DataDAL.SqlHelper
     /// <summary>
     /// 抽象数据库帮助类
     /// </summary>
-    public abstract  class SqlHelper
+    public abstract class SqlHelper
     {
+        #region 
+        /// <summary>
+        /// 
+        /// </summary>
+        private static readonly string ConnStrings = ConfigurationManager.ConnectionStrings["BeautySalonDataConn"].ConnectionString;
+        #endregion
 
+        #region 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private static SqlConnection CreateConn()
+        {
+            return new SqlConnection(ConnStrings);
+        }
 
+        #endregion
+
+        #region
+
+        public static DataTable GetDataTable(string sql, int cmdType, params SqlParameter[] cmdParms)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = CreateConn())
+            {
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+
+        #endregion
     }
 }
