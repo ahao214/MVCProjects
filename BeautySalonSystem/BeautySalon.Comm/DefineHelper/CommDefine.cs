db1a8956982684b0982253f406f27a7d3165d43c
liupenghao@fulan.com.cn <haochen214@163.com>
2023-12-31 22:37:50 +0800

    获取客户端的IP地址

---
+++ BeautySalonSystem/BeautySalon.Comm/DefineHelper/CommDefine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BeautySalon.Comm.DefineHelper
+{
+    /// <summary>
+    /// 公共函数
+    /// </summary>
+    public sealed class CommDefine
+    {
+        #region 获取客户端的IP地址
+        /// <summary>
+        /// 获取客户端的IP地址
+        /// </summary>
+        /// <returns></returns>
+        public static string GetClientIP()
+        {
+            string clientIP = string.Empty;
+            // 分代理和不代理,代理分普通匿名,高级匿名
+            clientIP = HttpContext.Current.Request.ServerVariables["HTTP_VIA"] == null ? HttpContext.Current.Request.UserHostAddress : HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            return clientIP;
+        }
+        #endregion
+
+    }
+}
