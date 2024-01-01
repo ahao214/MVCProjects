c59adb25b32ff6fa59ba4417076c358e61a1b388
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-01 16:57:15 +0800

    退出登录功能

--- BeautySalonSystem/BeautySalon/Controllers/HomeController.cs
+++ BeautySalonSystem/BeautySalon/Controllers/HomeController.cs
@@ -21,11 +21,6 @@ namespace BeautySalon.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
---
+++ BeautySalonSystem/BeautySalon/Controllers/UserAdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Razor.Generator;
+
+namespace BeautySalon.Controllers
+{
+    public class UserAdminController : Controller
+    {
+        // GET: UserAdmin
+        public ActionResult AccountInfo()
+        {
+            return View();
+        }
+
+        #region 退出登录
+        /// <summary>
+        /// 退出登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LogOut()
+        {
+            Session["LoginUser"] = null;
+            Session.Clear();
+            return base.Redirect("/");
+        }
+        #endregion
+
+    }
+}
