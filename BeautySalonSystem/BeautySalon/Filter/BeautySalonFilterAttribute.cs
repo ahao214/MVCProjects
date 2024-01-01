f61c90d8fc0783ebdb12de722e8d04676d359a50
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-01 14:38:36 +0800

    使用过滤器来进行后台权限验证

---
+++ BeautySalonSystem/BeautySalon/Filter/BeautySalonFilterAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautySalon.Filter
+{
+    /// <summary>
+    /// 过滤器
+    /// </summary>
+    public class BeautySalonFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (HttpContext.Current.Session["LoginUser"] is null)
+            {
+                HttpContext.Current.Response.Redirect("/");
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            base.OnResultExecuting(filterContext);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+
+    }
+}
