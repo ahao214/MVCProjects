c3a480310c00c3c626bb3acad1b18467d3172734
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-02 13:01:33 +0800

    登录账户信息

--- BeautySalonSystem/BeautySalon/Controllers/UserAdminController.cs
+++ BeautySalonSystem/BeautySalon/Controllers/UserAdminController.cs
@@ -1,4 +1,6 @@
 ﻿using BeautySalon.Filter;
+using BeautySalon.LogicBLL.TableBLL;
+using BeautySalon.Models.TableModel;
 using System.Web.Mvc;
 
 namespace BeautySalon.Controllers
@@ -6,11 +8,31 @@ namespace BeautySalon.Controllers
     [BeautySalonFilter] // 特性
     public class UserAdminController : Controller
     {
-        // GET: UserAdmin
+        #region 业务逻辑(用户)
+        /// <summary>
+        /// 业务逻辑(用户)
+        /// </summary>
+        private readonly UserAdminBLL userAdminBLL = new UserAdminBLL();
+        #endregion
+
+
+        #region 登录账户信息
+        /// <summary>
+        /// 登录账户信息
+        /// </summary>
+        /// <returns></returns>
         public ActionResult AccountInfo()
         {
-            return View();
+            UserAdmin userAdmin = new UserAdmin();
+            if (!(Session["LoginUser"] is null))
+            {
+                userAdmin = Session["LoginUser"] as UserAdmin;
+            }
+            userAdmin = userAdminBLL.GetUserAdminById(userAdmin.UserId);
+
+            return View(userAdmin);
         }
+        #endregion
 
         #region 修改密码
         /// <summary>
@@ -20,7 +42,7 @@ namespace BeautySalon.Controllers
         public ActionResult ModifyPass()
         {
             return View();
-        } 
+        }
         #endregion
 
         #region 退出登录
