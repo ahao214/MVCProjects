6d6fc78b24d716ba08302c7356ac0f85b694e08f
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-04 15:04:02 +0800

    项目管理页面

---
+++ BeautySalonSystem/BeautySalon/Controllers/ProjectController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautySalon.Controllers
+{
+    /// <summary>
+    /// 项目控制器
+    /// </summary>
+    public class ProjectController : Controller
+    {
+        #region 返回项目分类视图页面
+        /// <summary>
+        /// 返回项目分类视图页面
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        } 
+        #endregion
+
+
+
+    }
+}
